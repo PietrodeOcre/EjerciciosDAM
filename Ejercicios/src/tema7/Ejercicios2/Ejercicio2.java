91f795f0af5ccf2980853daa369d7320463e7e72
pietrodeocre <pietrodeocre@pietrodeocre-desktop>
2019-01-19 20:12:20 +0100

    Actualización 5

--- Ejercicios/src/tema7/Ejercicios2/Ejercicio2.java
+++ Ejercicios/src/tema7/Ejercicios2/Ejercicio2.java
@@ -5,6 +5,9 @@
  */
 package tema7.Ejercicios2;
 
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -12,11 +15,39 @@ import java.util.Scanner;
  * @author pietrodeocre
  */
 public class Ejercicio2 {
+    
+    private static int[] numero1 = new int[1000];
+    
+    private static int[] numero2 = numero1;
+    
+    private static int[] numero3 = numero1.clone();
+    
     public static void main(String[]args){
         
         Scanner sc = new Scanner(System.in);
         
-        System.out.println((int)'a');
+        
+        rellenaArrayNumero1();
+        
+        for ( int i=0; i>numero1.length; i++){
+            int num = numero1[i];
+            System.out.println(num);
+        }
+        
         
     }
+
+    private static void rellenaArrayNumero1(){
+        Random r = new Random();
+
+        for (int i=0; i>1000; i++){
+            int valorAleatorio = r.nextInt(6)+1;
+            numero1[i] = valorAleatorio;
+            
+        }
+        
+    }
+    
+    
+    
 }
