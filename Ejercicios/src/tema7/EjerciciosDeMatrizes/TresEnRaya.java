84f32c2eb3c70b0714ce4a7f3e0dee07aa027b47
pietrodeocre <pietrodeocre@pietrodeocre-desktop>
2019-02-02 22:20:09 +0100

    Actualización 02-02-19

--- Ejercicios/src/tema7/EjerciciosDeMatrizes/TresEnRaya.java
+++ Ejercicios/src/tema7/EjerciciosDeMatrizes/TresEnRaya.java
@@ -17,45 +17,74 @@ import java.util.Scanner;
  * @author pietrodeocre
  */
 public class TresEnRaya {
+    
+    //Creamos el tablero
+    //private static String[][] tablero = new String[3][3];
+    
     public static void main(String[] args){
+        //Memorizamos la posicion elegida por el jugador que corresponda
+        int[] num = new int[1];
+        
+        //Creamos el tablero
         String[][] tablero = new String[3][3];
         
-        
+        //Mostramos descripción y las normas del juego
         System.out.println("Este es el juego del 3 en raya.");
-        System.out.println("Escribe la fila y la columna (en ese orden) separado por un espacio");
+        System.out.println("Escribe un número del 0 al 2 para la fila e igual para la columna (en ese orden) separado por un espacio");
         
-        System.out.println(Arrays.toString(tablero));
+        //Pedimos la posicion
+        num = pedirPos();
+        
+        tableroPos(tablero,num[0],num[1]);
+        
+        //Mostramos el tablero
+        mostrar(tablero);
+        
         
     }
     
-    public static void jugador1(String[][] tablero, String jugador){
+    private static int[] pedirPos(){
         
+        int[] num = new int[2];
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Escribe la fila y la columna: ");
         String movimiento = sc.nextLine();
-        int num1 = 0, num2 = 0;
-        if ((movimiento.charAt(1)) == ' '){
-            if (movimiento.charAt(0) < 0 && movimiento.charAt(0) > 2){
-                num1 = Integer.valueOf(movimiento.charAt(0));
+        
+        if((Integer.parseInt(movimiento.substring(0,1)) == 0) || 
+                    (Integer.parseInt(movimiento.substring(0,1)) == 1) || 
+                    (Integer.parseInt(movimiento.substring(0,1)) == 2) && 
+                    (Integer.parseInt(movimiento.substring(2)) == 0) || 
+                    (Integer.parseInt(movimiento.substring(2)) == 1) ||
+                    (Integer.parseInt(movimiento.substring(2)) == 2) &&
+                    (movimiento.substring(1, 1).equals(" "))
+                    ){
+                num[0] = Integer.parseInt(movimiento.substring(0,1));
+                num[1] = Integer.parseInt(movimiento.substring(2));
+                return num;
+            }else{
+                System.out.println("Escribe un número del 0 al 2 para la fila e igual para la columna (en ese orden) separado por un espacio");
             }
-            if (movimiento.charAt(0) < 0 && movimiento.charAt(0) > 2){
-                num2 = Integer.valueOf(movimiento.charAt(2));
-            }
-        }
+   
+        return num;
+  
+    }
+    
+    private static String[][] tableroPos(String[][] tablero, int num1, int num2){
         
-        if (jugador.equals("1")){
-            tablero[num1][num2] = "0";
-        }else{
-             tablero[num1][num2] = "X";
-        }
+        tablero[num1][num2] = "0";
         
-        
+        return tablero;
     }
     
     
-    
-    public static void comprueba(String[][] tablero){
-        
+    public static void mostrar(String[][] tablero){
+        System.out.println("");
+        for (int i=0; i<tablero.length; i++){
+            for (int j=0;j<tablero[i].length; j++){
+                System.out.print("|"+tablero[i][j]+"|");
+            }
+            System.out.println("");
+        }
     }
 }
